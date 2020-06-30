95371c00197c59fbad4d096a2d251cd840d52931
Alice Perlov <aperlov23@gmail.com>
2020-06-30 03:11:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,4 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-  if board[index] == "  "
-    puts false
-end
+ ! (board[index] == "  " && board[index] == "" && board[index] == nil)
 end
